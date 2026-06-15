61492383c445c42c5b10233ead26e8a24f6c480b
Sahaya Jenika <jenika1806@gmail.com>
2026-06-15 18:54:26 +0530

    Updated dashboard login password

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const loginBtn=document.getElementById("loginBtn");
 loginBtn.addEventListener("click", function(){
     const username=document.getElementById("username").value;
     const password=document.getElementById("password").value;
-    if(username==="admin" && password==="admin123"){
+    if(username==="admin" && password==="jeni123"){
         localStorage.setItem("loggedIn","true");
         window.location.href="dashboard.html";
     }
