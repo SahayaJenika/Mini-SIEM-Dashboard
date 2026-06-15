9413f57dbe8b24c6134b72aaeeecd12a05397815
Sahaya Jenika <jenika1806@gmail.com>
2026-06-15 23:19:22 +0530

    Complete UI overhaul, added corporate theme, and project documentation

--- login.js
+++ login.js
@@ -1,12 +1,21 @@
-const loginBtn=document.getElementById("loginBtn");
+const loginBtn = document.getElementById("loginBtn");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
 loginBtn.addEventListener("click", function(){
-    const username=document.getElementById("username").value;
-    const password=document.getElementById("password").value;
-    if(username==="admin" && password==="jeni123"){
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    if(username === "admin" && password === "jeni123"){
         localStorage.setItem("loggedIn","true");
-        window.location.href="dashboard.html";
+        window.location.href = "dashboard.html";
     }
-else{
-    document.getElementById("message").innerText="Invalid Username or Password";
-}
+    else{
+        document.getElementById("message").innerText = "Invalid Username or Password";
+    }
 });
+function loginWithEnter(event){
+    if(event.key === "Enter"){
+        loginBtn.click();
+    }
+}
+usernameInput.addEventListener("keypress", loginWithEnter);
+passwordInput.addEventListener("keypress", loginWithEnter);
--- script.js
+++ script.js
@@ -219,15 +219,17 @@ function updateChart(){
         successCount,failureCount,lockedCount];
         eventChart.update();
 }
-    const themeBtn=document.getElementById("themeBtn");
-    themeBtn.addEventListener("click", function(){
-        document.body.classList.toggle("light-mode");
-        if(document.body.classList.contains("light-mode")){
-            themeBtn.innerText="Dark Mode";
-        }else{
-            themeBtn.innerText="Light Mode";
-        }
-    });
+    const themeBtn = document.getElementById("themeBtn");
+themeBtn.innerText = "Dark Mode"; 
+
+themeBtn.addEventListener("click", function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.innerText = "Light Mode";
+    } else {
+        themeBtn.innerText = "Dark Mode";
+    }
+});
    window.addEventListener("load", function () {
     setTimeout(() => {
         tour.drive();
